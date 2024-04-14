0b1c3de579b5e877b05c6ddf1ecbd7a8fb68f4a1
slinevitable2003 <slmozart2003@gmail.com>
2024-04-14 05:32:53 -0400

    Web structure establishment done.

--- IRA/views.py
+++ IRA/views.py
@@ -5,3 +5,11 @@ from IRA import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/query')
+def query():
+    return render_template('query.html')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
