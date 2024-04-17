65121ee1c308a6c3ff2a4bc61bca69581826da6d
slinevitable2003 <slmozart2003@gmail.com>
2024-04-17 08:19:07 -0400

    Ready to deploy.

--- IRA/__init__.py
+++ IRA/__init__.py
@@ -11,7 +11,7 @@ else:
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
